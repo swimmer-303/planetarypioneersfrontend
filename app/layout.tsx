cde4afdcc807a740f1734475ea80f17588d940d9
Spencer Hedges <8158452566@student.cms.k12.nc.us>
2025-10-05 12:44:53 -0400

    e

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} antialiased text-gray-100`}>
         <SupernovaProvider>
           <LoadingProvider>
             <CopyProtection />
